8d8b968642a39418c119fce79b0506d58b24c457
Andrea <xel23442@educastur.es>
2024-11-27 21:21:33 +0100

    commit cambios modelo

--- tarea3DWESandreafueyo/src/main/java/com/andreafueyo/tarea3DWESandreafueyo/modelo/Credenciales.java
+++ tarea3DWESandreafueyo/src/main/java/com/andreafueyo/tarea3DWESandreafueyo/modelo/Credenciales.java
@@ -1,5 +1,48 @@
 package com.andreafueyo.tarea3DWESandreafueyo.modelo;
 
-public class Credenciales {
+import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="credenciales")
+	public class Credenciales implements Serializable {
+
+
+		private static final long serialVersionUID = 1L;
+		
+		@Id
+		/*autoincrement*/
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+
+		@Column (unique = true)
+		private String usuario;
+		
+		@Column 
+		private String password;
+		
+		
+		public Credenciales() {}
+		
+		public Long getId() {
+			return id;
+		}
+		
+		public void setId(Long id) {
+			this.id = id;
+		}
+		
+		public String getUsuario() {
+			return usuario;
+		}
+		
+		public void setNombre(String password) {
+			this.password = password;
+	}
 }
--- tarea3DWESandreafueyo/src/main/java/com/andreafueyo/tarea3DWESandreafueyo/modelo/Persona.java
+++ tarea3DWESandreafueyo/src/main/java/com/andreafueyo/tarea3DWESandreafueyo/modelo/Persona.java
@@ -1,5 +1,48 @@
 package com.andreafueyo.tarea3DWESandreafueyo.modelo;
 
-public class Persona {
+import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+	@Entity
+	@Table(name="personas")
+
+	public class Persona implements Serializable {
+
+		private static final long serialVersionUID = 1L;
+		
+		@Id
+		/*autoincrement*/
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
+
+		@Column
+		private String nombre;
+		
+		@Column (unique = true)
+		private String email;
+		
+		
+		public Persona() {}
+		
+		public Long getId() {
+			return id;
+		}
+		
+		public void setId(Long id) {
+			this.id = id;
+		}
+		
+		public String getNombre() {
+			return nombre;
+		}
+		
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+	}
 }
